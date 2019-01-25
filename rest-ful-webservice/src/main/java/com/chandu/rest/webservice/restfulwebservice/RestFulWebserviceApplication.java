8c908bf580020b45e65b7b508c63665eeb4d9d5f
chandu3015 <chandu3015@gmail.com>
2019-01-25 01:22:54 -0600

    rest service start

---
+++ rest-ful-webservice/src/main/java/com/chandu/rest/webservice/restfulwebservice/RestFulWebserviceApplication.java
@@ -0,0 +1,14 @@
+package com.chandu.rest.webservice.restfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFulWebserviceApplication.class, args);
+	}
+
+}
+
